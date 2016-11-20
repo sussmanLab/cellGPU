09d94e2aacdfc79d58020235ea2857adc3619187
Daniel <dmsussman@gmail.com>
2016-11-20 19:15:07 +0000

    use fewer registers in gpu_force_sets

--- src/spv2d.cu
+++ src/spv2d.cu
@@ -132,6 +132,162 @@ __global__ void gpu_force_sets_kernel(float2      *d_points,
     Circumcenter(rij,rik,pno,vother);
 
 
+    float2 dAdv,dPdv;
+    float2 dEdv;
+    float  Adiff, Pdiff;
+    float2 dlast, dnext;
+    float  dlnorm,dnnorm;
+
+    //self terms
+    dAdv.x = 0.5*(vlast.y-vnext.y);
+    dAdv.y = 0.5*(vnext.x-vlast.x);
+    dlast.x = vlast.x-vcur.x;
+    dlast.y=vlast.y-vcur.y;
+    dlnorm = sqrt(dlast.x*dlast.x+dlast.y*dlast.y);
+    dnext.x = vcur.x-vnext.x;
+    dnext.y = vcur.y-vnext.y;
+    dnnorm = sqrt(dnext.x*dnext.x+dnext.y*dnext.y);
+    if(dnnorm < THRESHOLD)
+        dnnorm = THRESHOLD;
+    if(dlnorm < THRESHOLD)
+        dlnorm = THRESHOLD;
+    dPdv.x = dlast.x/dlnorm - dnext.x/dnnorm;
+    dPdv.y = dlast.y/dlnorm - dnext.y/dnnorm;
+    Adiff = KA*(d_AP[pidx].x - d_APpref[pidx].x);
+    Pdiff = KA*(d_AP[pidx].y - d_APpref[pidx].y);
+
+    dEdv.x  = 2.0*Adiff*dAdv.x +2.0*Pdiff*dPdv.x;
+    dEdv.y  = 2.0*Adiff*dAdv.y +2.0*Pdiff*dPdv.y;
+
+    //other terms...k first...
+    dAdv.x = 0.5*(vnext.y-vother.y);
+    dAdv.y = 0.5*(vother.x-vnext.x);
+    dlast.x = vnext.x-vcur.x;
+    dlast.y=vnext.y-vcur.y;
+    dlnorm = sqrt(dlast.x*dlast.x+dlast.y*dlast.y);
+    dnext.x = vcur.x-vother.x;
+    dnext.y = vcur.y-vother.y;
+    dnnorm = sqrt(dnext.x*dnext.x+dnext.y*dnext.y);
+    if(dnnorm < THRESHOLD)
+        dnnorm = THRESHOLD;
+    if(dlnorm < THRESHOLD)
+        dlnorm = THRESHOLD;
+    dPdv.x = dlast.x/dlnorm - dnext.x/dnnorm;
+    dPdv.y = dlast.y/dlnorm - dnext.y/dnnorm;
+    Adiff = KA*(d_AP[neighs.z].x - d_APpref[neighs.z].x);
+    Pdiff = KA*(d_AP[neighs.z].y - d_APpref[neighs.z].y);
+
+    dEdv.x  += 2.0*Adiff*dAdv.x +2.0*Pdiff*dPdv.x;
+    dEdv.y  += 2.0*Adiff*dAdv.y +2.0*Pdiff*dPdv.y;
+
+    //...and then j
+    dAdv.x = 0.5*(vother.y-vlast.y);
+    dAdv.y = 0.5*(vlast.x-vother.x);
+    dlast.x = vother.x-vcur.x;
+    dlast.y=vother.y-vcur.y;
+    dlnorm = sqrt(dlast.x*dlast.x+dlast.y*dlast.y);
+    dnext.x = vcur.x-vlast.x;
+    dnext.y = vcur.y-vlast.y;
+    dnnorm = sqrt(dnext.x*dnext.x+dnext.y*dnext.y);
+    if(dnnorm < THRESHOLD)
+        dnnorm = THRESHOLD;
+    if(dlnorm < THRESHOLD)
+        dlnorm = THRESHOLD;
+    dPdv.x = dlast.x/dlnorm - dnext.x/dnnorm;
+    dPdv.y = dlast.y/dlnorm - dnext.y/dnnorm;
+    Adiff = KA*(d_AP[neighs.y].x - d_APpref[neighs.y].x);
+    Pdiff = KA*(d_AP[neighs.y].y - d_APpref[neighs.y].y);
+
+    dEdv.x  += 2.0*Adiff*dAdv.x +2.0*Pdiff*dPdv.x;
+    dEdv.y  += 2.0*Adiff*dAdv.y +2.0*Pdiff*dPdv.y;
+
+    d_forceSets[n_idx(nn,pidx)] = dEdv*dhdr;
+
+//    if(pidx == 0) printf("(%f,%f)\t(%f,%f)\t(%f,%f)\n",dPidv.x,dPidv.y,dPkdv.x,dPkdv.y,dPjdv.x,dPjdv.y);
+    //if(pidx == 0) printf("%i %f %f\n",nn,temp.x,temp.y);
+//    if(pidx == 0) printf("%f\t%f\t%f\t%f\n",dhdr.x11,dhdr.x12,dhdr.x21,dhdr.x22);
+
+    return;
+    };
+
+__global__ void gpu_force_sets_tensions_kernel(float2      *d_points,
+                                          int     *d_nn,
+                                          float2  *d_AP,
+                                          float2  *d_APpref,
+                                          int4    *d_delSets,
+                                          int     *d_delOther,
+                                          float2  *d_forceSets,
+                                          int     *d_cellTypes,
+                                          float   KA,
+                                          float   KP,
+                                          float   gamma,
+                                          int     computations,
+                                          int     neighMax,
+                                          Index2D n_idx,
+                                          gpubox Box
+                                        )
+    {
+    unsigned int tidx = blockDim.x * blockIdx.x + threadIdx.x;
+    if (tidx >= computations)
+        return;
+    //which particle are we evaluating, and which neighbor
+    int pidx = tidx / neighMax;
+    int nn = tidx - pidx*neighMax;
+    //how many neighbors does it have?
+    int pNeighbors = d_nn[pidx];
+
+    if(nn >=pNeighbors)
+        return;
+    //Great...access the four Delaunay neighbors and the relevant fifth point
+    float2 pi   = d_points[pidx];
+
+    int4 neighs = d_delSets[n_idx(nn,pidx)];
+    float2 pnm2,rij, rik,pn2,pno;
+//if(true)    printf("tidx:%i,   pidx:%i,   nm %i,  %i %i %i %i \n",tidx,pidx,neighMax,neighs.x,neighs.y,neighs.z,neighs.w);
+
+    Box.minDist(d_points[neighs.x],pi,pnm2);
+    Box.minDist(d_points[neighs.y],pi,rij);
+    Box.minDist(d_points[neighs.z],pi,rik);
+    Box.minDist(d_points[neighs.w],pi,pn2);
+    Box.minDist(d_points[d_delOther[n_idx(nn,pidx)]],pi,pno);
+
+    //first, compute the derivative of the main voro point w/r/t pidx's position
+    //pnm1 is rij, pn1 is rik
+    Matrix2x2 dhdr;
+    Matrix2x2 Id;
+    float2 rjk;
+    rjk.x =rik.x-rij.x;
+    rjk.y =rik.y-rij.y;
+    float2 dbDdri,dgDdri,dDdriOD,z;
+    float betaD = -dot(rik,rik)*dot(rij,rjk);
+    float gammaD = dot(rij,rij)*dot(rik,rjk);
+    float cp = rij.x*rjk.y - rij.y*rjk.x;
+    float D = 2*cp*cp;
+    z.x = betaD*rij.x+gammaD*rik.x;
+    z.y = betaD*rij.y+gammaD*rik.y;
+
+    dbDdri.x = 2*dot(rij,rjk)*rik.x+dot(rik,rik)*rjk.x;
+    dbDdri.y = 2*dot(rij,rjk)*rik.y+dot(rik,rik)*rjk.y;
+
+    dgDdri.x = -2*dot(rik,rjk)*rij.x-dot(rij,rij)*rjk.x;
+    dgDdri.y = -2*dot(rik,rjk)*rij.y-dot(rij,rij)*rjk.y;
+
+    dDdriOD.x = (-2.0*rjk.y)/cp;
+    dDdriOD.y = (2.0*rjk.x)/cp;
+
+    dhdr = Id+1.0/D*(dyad(rij,dbDdri)+dyad(rik,dgDdri)-(betaD+gammaD)*Id-dyad(z,dDdriOD));
+
+
+
+    //finally, compute all of the forces
+    float2 origin; origin.x = 0.0;origin.y=0.0;
+    float2 vlast,vcur,vnext,vother;
+    Circumcenter(origin,pnm2,rij,vlast);
+    Circumcenter(origin,rij,rik,vcur);
+    Circumcenter(origin,rik,pn2,vnext);
+    Circumcenter(rij,rik,pno,vother);
+
+
     float2 dAidv,dPidv;
     float2 dAjdv,dPjdv;
     float2 dAkdv,dPkdv;
@@ -213,7 +369,6 @@ __global__ void gpu_force_sets_kernel(float2      *d_points,
 
     return;
     };
-
 
 
 
@@ -504,7 +659,7 @@ bool gpu_force_sets_tensions(float2 *d_points,
     if (computations < 128) block_size = 32;
     unsigned int nblocks  = computations/block_size + 1;
 
-    gpu_force_sets_kernel<<<nblocks,block_size>>>(
+    gpu_force_sets_tensions_kernel<<<nblocks,block_size>>>(
                                                 d_points,
                                                 d_nn,
                                                 d_AP,
@@ -512,8 +667,10 @@ bool gpu_force_sets_tensions(float2 *d_points,
                                                 d_delSets,
                                                 d_delOther,
                                                 d_forceSets,
+                                                d_cellTypes,
                                                 KA,
                                                 KP,
+                                                gamma,
                                                 computations,
                                                 neighMax,
                                                 n_idx,
