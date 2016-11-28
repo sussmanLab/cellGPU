558c778bf2f4fd991d76e5753512946bf312a625
Daniel <dmsussman@gmail.com>
2016-11-28 13:32:02 +0000

    re-wrote indexer class

--- inc/indexer.h
+++ inc/indexer.h
@@ -1,115 +1,44 @@
-/*
-Highly Optimized Object-oriented Many-particle Dynamics -- Blue Edition
-(HOOMD-blue) Open Source Software License Copyright 2009-2016 The Regents of
-the University of Michigan All rights reserved.
-
-HOOMD-blue may contain modifications ("Contributions") provided, and to which
-copyright is held, by various Contributors who have granted The Regents of the
-University of Michigan the right to modify and/or distribute such Contributions.
-
-You may redistribute, use, and create derivate works of HOOMD-blue, in source
-and binary forms, provided you abide by the following conditions:
-
-* Redistributions of source code must retain the above copyright notice, this
-list of conditions, and the following disclaimer both in the code and
-prominently in any materials provided with the distribution.
-
-* Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions, and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-* All publications and presentations based on HOOMD-blue, including any reports
-or published results obtained, in whole or in part, with HOOMD-blue, will
-acknowledge its use according to the terms posted at the time of submission on:
-http://codeblue.umich.edu/hoomd-blue/citations.html
-
-* Any electronic documents citing HOOMD-Blue will link to the HOOMD-Blue website:
-http://codeblue.umich.edu/hoomd-blue/
-
-* Apart from the above required attributions, neither the name of the copyright
-holder nor the names of HOOMD-blue's contributors may be used to endorse or
-promote products derived from this software without specific prior written
-permission.
-
-Disclaimer
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS'' AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND/OR ANY
-WARRANTIES THAT THIS SOFTWARE IS FREE OF INFRINGEMENT ARE DISCLAIMED.
-
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 #ifndef INDEXER
 #define INDEXER
 
 
 #ifdef NVCC
-#define HOSTDEVICE __host__ __device__
+#define HOSTDEVICE __host__ __device__ inline
 #else
-#define HOSTDEVICE
+#define HOSTDEVICE inline __attribute__((always_inline))
 #endif
 
-// CREDIT HOOMD-BLUE FOR THIS
+//a class for converting between a 2d index and a 1-d array
 
-//! Index a 2D array
-/*! Row major mapping of 2D onto 1D
-    \ingroup utils
-*/
-struct Index2D
+class Index2D
     {
     public:
-        //! Contstructor
-        /*! \param w Width of the square 2D array
-        */
-        HOSTDEVICE inline Index2D(unsigned int w=0) : m_w(w), m_h(w) {}
+        HOSTDEVICE Index2D(unsigned int w=0) : width(w), height(w) {}
+        HOSTDEVICE Index2D(unsigned int w, unsigned int h) : width(w), height(h) {}
 
-        //! Contstructor
-        /*! \param w Width of the rectangular 2D array
-            \param h Height of the rectangular 2D array
-        */
-        HOSTDEVICE inline Index2D(unsigned int w, unsigned int h) : m_w(w), m_h(h) {}
-
-        //! Calculate an index
-        /*! \param i column index
-            \param j row index
-            \returns 1D array index corresponding to the 2D index (\a i, \a j) in row major order
-        */
-        HOSTDEVICE inline unsigned int operator()(unsigned int i, unsigned int j) const
+        HOSTDEVICE unsigned int operator()(unsigned int i, unsigned int j) const
             {
-            return j*m_w + i;
+            return j*width + i;
             }
 
-        //! Get the number of 1D elements stored
-        /*! \returns Number of elements stored in the underlying 1D array
-        */
-        HOSTDEVICE inline unsigned int getNumElements() const
+        HOSTDEVICE unsigned int getNumElements() const
             {
-            return m_w*m_h;
+            return width*height;
             }
 
-        //! Get the width of the 2D array
-        HOSTDEVICE inline unsigned int getW() const
+        HOSTDEVICE unsigned int getW() const
             {
-            return m_w;
+            return width;
             }
 
-        //! Get the height of the 2D array
-        HOSTDEVICE inline unsigned int getH() const
+        HOSTDEVICE unsigned int getH() const
             {
-            return m_h;
+            return height;
             }
 
     private:
-        unsigned int m_w;   //!< Width of the 2D array
-        unsigned int m_h;   //!< Height of the 2D array
+        unsigned int width;   // array width
+        unsigned int height;   // array height
     };
 
 #undef HOSTDEVICE
