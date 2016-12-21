3128ca426d7ce9e5daad8106c1137573b2ba3e4f
Daniel Sussman <dmsussman@gmail.com>
2016-12-21 09:00:38 -0500

    allow reading excluded forces if present

--- voroguppy.cpp
+++ voroguppy.cpp
@@ -91,6 +91,18 @@ int main(int argc, char*argv[])
     spv.setv0Dr(v0,1.0);
     spv.setDeltaT(dt);
 
+    if(program_switch == -3)
+        {
+        sprintf(dataname,"/hdd2/data/spv/Plates/Plates_N5000_p4.000_v0.100_Dr1.000_g0.100.nc");
+        SPVDatabase ncdat(numpts,dataname,NcFile::ReadOnly);
+        for (int rr = 0; rr <ncdat.GetNumRecs(); ++rr)
+            {
+            ncdat.ReadState(spv,rr);
+            cout << "frame " << rr << "  q= " <<  spv.reportq() << endl;
+            };
+        return 0;
+
+        };
     if(program_switch == -2)
         {
         sprintf(dataname,"/hdd2/data/spv/MSD/monodisperse_N5000_p3.84_v0.01_Dr1.000.nc");
