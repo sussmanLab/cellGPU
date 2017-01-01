fb282e93c71a1dcbb74e3a3771123e15cb4d1377
Daniel <dsussman@sas.upenn.edu>
2017-01-01 15:01:36 -0500

    nudge

--- activeVertex.cpp
+++ activeVertex.cpp
@@ -69,6 +69,7 @@ int main(int argc, char*argv[])
         if(USE_GPU >= 0)
             {
             avm.computeGeometryGPU();
+            avm.computeForcesGPU();
             }
         else
             {
