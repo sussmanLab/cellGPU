87539fc5901477c7795b8869ec8392fd313c5655
Daniel Sussman <dmsussma@syr.edu>
2017-09-03 21:21:00 -0400

    documented example cpp files in main directory better

--- activeVertex.cpp
+++ activeVertex.cpp
@@ -18,32 +18,28 @@ along with a range of v0 and p0. This model is not very sensitive to these other
 */
 int main(int argc, char*argv[])
 {
-    int numpts = 200;
-    int USE_GPU = 0;
-    int USE_TENSION = 0;
+    int numpts = 200; //number of cells
+    int USE_GPU = 0; //0 or greater uses a gpu, any negative number runs on the cpu
     int c;
-    int tSteps = 5;
-    int initSteps = 0;
+    int tSteps = 5; //number of time steps to run after initialization
+    int initSteps = 1; //number of initialization steps
 
-    Dscalar dt = 0.01;
-    Dscalar p0 = 3.84;
-    Dscalar a0 = 1.0;
-    Dscalar v0 = 0.01;
+    Dscalar dt = 0.01; //the time step size
+    Dscalar p0 = 4.0;  //the preferred perimeter
+    Dscalar a0 = 1.0;  // the preferred area
+    Dscalar v0 = 0.1;  // the self-propulsion
     Dscalar Dr = 1.0;
-    Dscalar gamma = 0.0;
 
-    int program_switch = 0;
+    int program_switch = 0; //various settings control output
     while((c=getopt(argc,argv,"n:g:m:s:r:a:i:v:b:x:y:z:p:t:e:d:")) != -1)
         switch(c)
         {
             case 'n': numpts = atoi(optarg); break;
             case 't': tSteps = atoi(optarg); break;
             case 'g': USE_GPU = atoi(optarg); break;
-            case 'x': USE_TENSION = atoi(optarg); break;
             case 'i': initSteps = atoi(optarg); break;
             case 'z': program_switch = atoi(optarg); break;
             case 'e': dt = atof(optarg); break;
-            case 's': gamma = atof(optarg); break;
             case 'p': p0 = atof(optarg); break;
             case 'a': a0 = atof(optarg); break;
             case 'v': v0 = atof(optarg); break;
@@ -59,9 +55,9 @@ int main(int argc, char*argv[])
             default:
                        abort();
         };
-    clock_t t1,t2;
-
-    bool reproducible = true;
+    clock_t t1,t2; //clocks for timing information
+    bool reproducible = true; // if you want random numbers with a more random seed each run, set this to false
+    //check to see if we should run on a GPU
     bool initializeGPU = true;
     if (USE_GPU >= 0)
         {
@@ -71,39 +67,41 @@ int main(int argc, char*argv[])
         }
     else
         initializeGPU = false;
-
+    
+    //possibly save output in netCDF format
     char dataname[256];
     sprintf(dataname,"../test.nc");
     int Nvert = 2*numpts;
     AVMDatabaseNetCDF ncdat(Nvert,dataname,NcFile::Replace);
 
-    bool runSPV = false;
+    bool runSPV = false;//setting this to true will relax the random cell positions to something more uniform before running vertex model dynamics
 
+    //define an equation of motion object...here for self-propelled cells
     EOMPtr spp = make_shared<selfPropelledCellVertexDynamics>(numpts,Nvert);
+    //define a vertex model configuration with a quadratic energy functional
     shared_ptr<AVM2D> avm = make_shared<AVM2D>(numpts,1.0,4.0,reproducible,runSPV);
+    //set the cell preferences to uniformly have A_0 = 1, P_0 = p_0
     avm->setCellPreferencesUniform(1.0,p0);
+    //set the cell activity to have D_r = 1. and a given v_0
     avm->setv0Dr(v0,1.0);
-
+    //when an edge gets less than this long, perform a simple T1 transition
     avm->setT1Threshold(0.04);
 
-
+    //combine the equation of motion and the cell configuration in a "Simulation"
     SimulationPtr sim = make_shared<Simulation>();
     sim->setConfiguration(avm);
     sim->addUpdater(spp,avm);
+    //set the time step size
     sim->setIntegrationTimestep(dt);
+    //initialize Hilbert-curve sorting... can be turned off by commenting out this line or seting the argument to a negative number
 //    sim->setSortPeriod(initSteps/10);
     //set appropriate CPU and GPU flags
     sim->setCPUOperation(!initializeGPU);
     sim->setReproducible(reproducible);
+    //perform some initial time steps. If program_switch < 0, save periodically to a netCDF database
     for (int timestep = 0; timestep < initSteps+1; ++timestep)
         {
         sim->performTimestep();
-        if(timestep%((int)(1/dt))==0)
-            {
-    //        cout << timestep << endl;
-    //        avm.reportAP();
-    //        avm.reportMeanVertexForce();
-            };
         if(program_switch <0 && timestep%((int)(1/dt))==0)
             {
             cout << timestep << endl;
@@ -111,8 +109,8 @@ int main(int argc, char*argv[])
             };
         };
 
+    //run for additional timesteps, and record timing information. Save frames to a database if desired
     t1=clock();
-
     for (int timestep = 0; timestep < tSteps; ++timestep)
         {
         sim->performTimestep();
@@ -125,13 +123,10 @@ int main(int argc, char*argv[])
 
     t2=clock();
     cout << "timestep time per iteration currently at " <<  (t2-t1)/(Dscalar)CLOCKS_PER_SEC/tSteps << endl << endl;
-
     avm->reportMeanVertexForce();
     cout << "Mean q = " << avm->reportq() << endl;
 
-
     if(initializeGPU)
         cudaDeviceReset();
-
     return 0;
     };
--- voronoi.cpp
+++ voronoi.cpp
@@ -16,31 +16,27 @@ model of active motility
 */
 int main(int argc, char*argv[])
 {
-    int numpts = 200;
-    int USE_GPU = 0;
-    int USE_TENSION = 0;
+    //...some default parameters
+    int numpts = 200; //number of cells
+    int USE_GPU = 0; //0 or greater uses a gpu, any negative number runs on the cpu
     int c;
-    int tSteps = 5;
-    int initSteps = 0;
+    int tSteps = 5; //number of time steps to run after initialization
+    int initSteps = 1; //number of initialization steps
 
-    Dscalar dt = 0.1;
-    Dscalar p0 = 4.0;
-    Dscalar a0 = 1.0;
-    Dscalar v0 = 0.1;
-    Dscalar gamma = 0.0;
+    Dscalar dt = 0.01; //the time step size
+    Dscalar p0 = 4.0;  //the preferred perimeter
+    Dscalar a0 = 1.0;  // the preferred area
+    Dscalar v0 = 0.1;  // the self-propulsion
 
-    int program_switch = 0;
+    //The defaults can be overridden from the command line
     while((c=getopt(argc,argv,"n:g:m:s:r:a:i:v:b:x:y:z:p:t:e:")) != -1)
         switch(c)
         {
             case 'n': numpts = atoi(optarg); break;
             case 't': tSteps = atoi(optarg); break;
             case 'g': USE_GPU = atoi(optarg); break;
-            case 'x': USE_TENSION = atoi(optarg); break;
             case 'i': initSteps = atoi(optarg); break;
-            case 'z': program_switch = atoi(optarg); break;
             case 'e': dt = atof(optarg); break;
-            case 's': gamma = atof(optarg); break;
             case 'p': p0 = atof(optarg); break;
             case 'a': a0 = atof(optarg); break;
             case 'v': v0 = atof(optarg); break;
@@ -56,8 +52,9 @@ int main(int argc, char*argv[])
                        abort();
         };
 
-    clock_t t1,t2;
-    bool reproducible = true;
+    clock_t t1,t2; //clocks for timing information
+    bool reproducible = true; // if you want random numbers with a more random seed each run, set this to false
+    //check to see if we should run on a GPU
     bool initializeGPU = true;
     if (USE_GPU >= 0)
         {
@@ -68,36 +65,41 @@ int main(int argc, char*argv[])
     else
         initializeGPU = false;
 
+    //define an equation of motion object...here for self-propelled cells
     EOMPtr spp = make_shared<selfPropelledParticleDynamics>(numpts);
+    //define a voronoi configuration with a quadratic energy functional
     shared_ptr<Voronoi2D> spv  = make_shared<Voronoi2D>(numpts,1.0,4.0,reproducible);
 
+    //set the cell preferences to uniformly have A_0 = 1, P_0 = p_0
     spv->setCellPreferencesUniform(1.0,p0);
+    //set the cell activity to have D_r = 1. and a given v_0
     spv->setv0Dr(v0,1.0);
 
+    //combine the equation of motion and the cell configuration in a "Simulation"
     SimulationPtr sim = make_shared<Simulation>();
     sim->setConfiguration(spv);
     sim->addUpdater(spp,spv);
+    //set the time step size
     sim->setIntegrationTimestep(dt);
+    //initialize Hilbert-curve sorting... can be turned off by commenting out this line or seting the argument to a negative number
     sim->setSortPeriod(initSteps/10);
     //set appropriate CPU and GPU flags
     sim->setCPUOperation(!initializeGPU);
     sim->setReproducible(reproducible);
-    //initialize parameters
 
-    char dataname[256];
-    sprintf(dataname,"../test.nc");
-
-
+    //run for a few initialization timesteps
     printf("starting initialization\n");
     for(int ii = 0; ii < initSteps; ++ii)
         {
         sim->performTimestep();
         };
-
+    
     printf("Finished with initialization\n");
-    //cout << "current q = " << spv.reportq() << endl;
+    cout << "current q = " << spv->reportq() << endl;
+    //the reporting of the force should yield a number that is numerically close to zero.
     spv->reportMeanCellForce(false);
 
+    //run for additional timesteps, and record timing information
     t1=clock();
     for(int ii = 0; ii < tSteps; ++ii)
         {
@@ -113,9 +115,7 @@ int main(int argc, char*argv[])
     cout << "timestep ~ " << steptime << " per frame; " << endl;
     cout << spv->reportq() << endl;
 
-
     if(initializeGPU)
         cudaDeviceReset();
-
     return 0;
 };
