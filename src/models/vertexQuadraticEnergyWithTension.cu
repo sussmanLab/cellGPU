dfe16db0df13f7511d328026394737c0505e9ac6
Daniel Sussman <dmsussma@syr.edu>
2018-01-15 09:47:38 -0500

    added blank file for GPU vertex tension class

---
+++ src/models/vertexQuadraticEnergyWithTension.cu
@@ -0,0 +1,18 @@
+#define NVCC
+#define ENABLE_CUDA
+
+#include <cuda_runtime.h>
+#include "vertexQuadraticEnergyWithTension.cuh"
+
+/** \file vertexQuadraticEnergyWithTension.cu
+    * Defines kernel callers and kernels for GPU calculations of vertex models
+*/
+
+/*!
+    \addtogroup vmKernels
+    @{
+*/
+
+
+/** @} */ //end of group declaration
+
